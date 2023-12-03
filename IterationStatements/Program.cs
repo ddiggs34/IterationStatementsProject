b7f3e632cbfb2b912b248ea84bc50c263e2db8c8
David Diggs <ddiggs34@gmail.com>
2023-12-02 23:10:03 -0500

    done

--- IterationStatements/Program.cs
+++ IterationStatements/Program.cs
@@ -7,66 +7,89 @@ namespace IterationStatements
     {
         static void Main(string[] args)
         {
-                //TODO - Read each comment and complete its instruction
-                // like the example below
 
-                //Create a List called "numbers" - DONE!
-                var numbers = new List<int>();
-            
+            //for (int i = 0; i <= 5; i++)
+            //{
+            //    Console.WriteLine($"{i}");
+            //}
+
+            //TODO - Read each comment and complete its instruction
+            // like the example below
+
+            //Create a List called "numbers" - DONE!
+            var numbers = new List<int>();
+
             //-----START HERE------------------------------------------
             //Create a variable of type int and name it num
             //initialize the variable with a value of 0
-
-
+            var num = 0;
 
             // Create a do-while loop and use the template below:
 
             {
                 // Increment num by 1
+                do
+                {
+                    num++;
+                    numbers.Add(num);
+                } while (num < 100);
 
                 // Then add num to the collection - numbers
-                    // Hint: reference num inside of the Add method's parentheses
-                numbers.Add(/* num */);
+                // Hint: reference num inside of the Add method's parentheses
 
-            } // <---- While your variable is less than 100
-
+                // <---- While your variable is less than 100
 
 
-            // Create a while loop
-            // <--- While num is less than 200
-            {
-                // Increment num by 1
-                // Then add num to the collection numbers
+
+                // Create a while loop
+                while (num < 200)
+                {
+                    num++;
+
+                    numbers.Add(num);
+                }
+                // <--- While num is less than 200
+                {
+                    // Increment num by 1
+                    // Then add num to the collection numbers
                     //HINT: copy how this was done in the do while loop
 
-            }
+                }
 
 
-            // This is to show the user that the numbers will start increasing on the console
-            Console.WriteLine("Increase:");
+                // This is to show the user that the numbers will start increasing on the console
+                Console.WriteLine("Increase:");
 
 
-            // Create a foreach loop using the collection - numbers
+                // Create a foreach loop using the collection - numbers
+
+                foreach (var number in numbers)
                 //In the scope of the foreach loop, print each number in numbers
-             
+                {
+                    Console.WriteLine(number);
+                }
 
 
-            Console.WriteLine("");
-            Console.WriteLine("Decrease:");
+                Console.WriteLine("------ DECREASE -----------");
+                Console.WriteLine(" ");
 
 
-            // Create a for loop - this will print the numbers in reverse order - from 200 to 1
+                // Create a for loop - this will print the numbers in reverse order - from 200 to 1
                 // in your initializer set the value of i to 199
                 // in your conditional, as long as i is less than or equal to the amount of items in "numbers" - use (numbers.Count)
                 // AND as long as i is greater than or equal to 0
                 // Decrement i by 1
 
-            //start for loop here
-            {
-                // place numbers[i] inside of the Console.WriteLine() method
-            }
+                //start for loop here
+                
+                    for (int i = 199; i <= numbers.Count && i >= 0; i--) 
+                    {
+                        Console.WriteLine($"{i}");
+                    }
+                
 
-            //------------End of exercise
+                //------------End of exercise
+            }
         }
     }
 }
